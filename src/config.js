96469b9794aa7aa7c018d7f0453cc880f9a17058
Jaydeep Gondaliya <jaydeepgondaliya182@gmail.com>
2025-11-16 05:31:57 -0800

    api integation

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 // API configuration
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
+const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "hhttps://post-meeting-generator-gold.vercel.app";
 
 export { API_URL };
